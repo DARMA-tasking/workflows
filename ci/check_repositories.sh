51b3b0c73b4624124732b4e0334c1f7d2f79a75c
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-09-18 14:30:37 +0200

    #1: update ci script

--- ci/check_repositories.sh
+++ ci/check_repositories.sh
@@ -4,6 +4,7 @@ CURRENT_DIR="$(dirname -- "$(realpath -- "$0")")" # Current directory
 PARENT_DIR="$(dirname "$CURRENT_DIR")"
 WORKING_DIR="$PARENT_DIR/output"
 ORG=DARMA-tasking
+
 # Clean
 rm -rf $WORKING_DIR
 mkdir -p $WORKING_DIR
@@ -31,10 +32,9 @@ check-pr-fixes-issue \
 action-git-diff-check \
 )
 
+N_ERRORS=0
 for e in "${DARMA_REPOSITORIES[@]}"
-do
-    printf "%s\n" "${e}"
-    
+do    
     if [[ " ${CHECKS_REPOS[*]} " =~ [[:space:]]${e}[[:space:]] ]]; then
         echo "$ORG/$e > Ignoring (checks repository)";
         echo "--------------------------------------------------";
@@ -42,6 +42,7 @@ do
         echo "$ORG/$e > Ignoring (excluded)";
         echo "--------------------------------------------------";
     else
+        N_REPO_ERRORS=0
         TSSTART=$(date +%s)
         echo "$ORG/$e > Cloning repository...";
         git clone https://github.com/$ORG/$e $WORKING_DIR/$e >/dev/null 2>&1
@@ -49,14 +50,26 @@ do
         do
             if [ ! -f "$WORKING_DIR/$e/.github/workflows/$w.yml" ]; then
                 echo "[error] Missing workflow file '$w.yml' at $WORKING_DIR/$e/.github/workflows/$w.yml"
+                $((N_ERRORS++))
+                $((N_REPO_ERRORS++))
             else
                 echo "[ok] workflow file '$w.yml' OK"
             fi
         done
         TSEND=$(date +%s)
         TSDURATION=$(( $TSEND - $TSSTART ))
+        if [[ $N_REPO_ERRORS -gt 0 ]]
+        then
+            echo "$WORKING_DIR/$e has $N_REPO_ERRORS errors."
+        else
+            echo "[success] repository checks OK."
+        fi
         echo "$WORKING_DIR/$e has been processed in $TSDURATION seconds."
         echo "--------------------------------------------------";
     fi
 done
 
+if [[ $N_ERRORS -gt 0 ]]
+then
+    exit(1)
+end
