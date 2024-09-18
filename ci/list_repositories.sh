13a8565a0fdc40ffd694968e4bb8cbfa64fa6803
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-09-18 17:04:19 +0200

    #1: resolve exclusion of some repositories from checks

--- ci/list_repositories.sh
+++ ci/list_repositories.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-CURRENT_DIR="$(dirname -- "$(realpath -- "$0")")" # Current directory
-PARENT_DIR="$(dirname "$CURRENT_DIR")"
-WORKING_DIR="$PARENT_DIR/output"
 ORG=DARMA-tasking
-
-EXCLUDE='[
-    "DARMA-tasking.github.io",
-    "find-unsigned-commits",
-    "check-commit-format",
-    "find-trailing-whitespace",
-    "check-pr-fixes-issue"
-]'
-JQ=$EXCLUDE' as $blacklist | .[] | select( .name as $in | $blacklist | index($in) | not)'
-gh repo list DARMA-tasking --json name,defaultBranchRef --jq "$JQ" --jq 'sort_by(.name)' | jq
+EXCLUDE="[\"DARMA-tasking.github.io\",\"find-unsigned-commits\",\"check-commit-format\",\"find-trailing-whitespace\",\"check-pr-fixes-issue\",\"workflows\"]"
+JQ="$EXCLUDE as \$blacklist | .[] | select( .name as \$in | \$blacklist | index(\$in) | not)"
+gh repo list $ORG --json name,defaultBranchRef --jq "$JQ" | jq -s 'sort_by(.name)'
