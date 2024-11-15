414ec53671b5256a577067580191d39fedc77525
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-11-15 18:15:34 +0100

    #2: replace flang-16 dep by flang-new-16 to try a fix and update nvcc build dir

--- ci/shared/scripts/deps/nvcc_wrapper.sh
+++ ci/shared/scripts/deps/nvcc_wrapper.sh
@@ -2,9 +2,9 @@
 
 set -exo pipefail
 
-mkdir -p /opt/nvcc_wrapper/bin
+mkdir -p /opt/nvcc_wrapper/build
 
-wget https://raw.githubusercontent.com/kokkos/kokkos/master/bin/nvcc_wrapper -P /opt/nvcc_wrapper/bin
-chmod +x /opt/nvcc_wrapper/bin/nvcc_wrapper
+wget https://raw.githubusercontent.com/kokkos/kokkos/master/bin/nvcc_wrapper -P /opt/nvcc_wrapper/build
+chmod +x /opt/nvcc_wrapper/build/nvcc_wrapper
 
 which nvcc_wrapper
