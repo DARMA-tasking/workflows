4788af30e5c429688bcfa5503302581e29b6c3ca
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-12-12 10:28:02 +0100

    #2: add gcovr to alpine image

--- ci/shared/scripts/setup-amd64-alpine-clang-13-cpp.sh
+++ ci/shared/scripts/setup-amd64-alpine-clang-13-cpp.sh
@@ -84,7 +84,7 @@ wget $SCRIPTS_DEPS_URL/mpich.sh
 # 2. Install dependency
 chmod u+x *.sh
 ls -l
-./packages.sh "alpine-sdk" "autoconf" "automake" "binutils-dev" "ccache" "cmake" "dpkg" "libdwarf-dev" "libunwind-dev" "libtool" "linux-headers" "m4" "make" "ninja" "zlib" "zlib-dev" "python3" "clang" "clang-dev"
+./packages.sh "alpine-sdk" "autoconf" "automake" "binutils-dev" "ccache" "cmake" "dpkg" "libdwarf-dev" "libunwind-dev" "libtool" "linux-headers" "m4" "make" "ninja" "zlib" "zlib-dev" "python3" "gcovr" "clang" "clang-dev"
 CC="clang" CXX="clang++" ./mpich.sh "3.3.2" "-j4"
 
 # Remove install scripts
