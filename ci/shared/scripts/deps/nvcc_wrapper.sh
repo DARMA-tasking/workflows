e3e96258eeb6a194a5f119305c102c5f80aa9cca
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2025-04-02 17:37:47 +0200

    #18: wget: use `--no-verbose`

--- ci/shared/scripts/deps/nvcc_wrapper.sh
+++ ci/shared/scripts/deps/nvcc_wrapper.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 
 mkdir -p /opt/nvcc_wrapper/build
 
-wget https://raw.githubusercontent.com/kokkos/kokkos/master/bin/nvcc_wrapper -P /opt/nvcc_wrapper/build
+wget --no-verbose https://raw.githubusercontent.com/kokkos/kokkos/master/bin/nvcc_wrapper -P /opt/nvcc_wrapper/build
 chmod +x /opt/nvcc_wrapper/build/nvcc_wrapper
 
 which nvcc_wrapper
